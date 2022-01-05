04f3c495d434a6129fbd82f0f3fda742ba511789
Guilherme Viana <gguicido.viana@gmail.com>
2022-01-05 02:53:53 -0300

    projeto finalizado

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php
-include 'phpmailer/src/PHPMailer.php';
-include 'phpmailer/src/SMTP.php';
-include 'phpmailer/src/Exception.php';
+include './phpmailer/src/PHPMailer.php';
+include './phpmailer/src/SMTP.php';
+include './phpmailer/src/Exception.php';
 
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
@@ -12,50 +12,60 @@ use PHPMailer\PHPMailer\Exception;
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['subject'];
+$message = $_POST['message'];
+$data = date('d/m/Y H:i:s');
+
 try {
+
     //Server settings
-	
-	//Comente ou descomente essa linha para debugar erros de envio de email (descomentar irá mostrar as mensagens de erro)
+
+    //Comente ou descomente essa linha para debugar erros de envio de email (descomentar irá mostrar as mensagens de erro)
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-	
+
     $mail->isSMTP();
     $mail->Debugoutput = 'html';                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host = 'smtp.hostinger.com' ;              //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'gguicido.viana@gmail.com';                     //SMTP username
-    $mail->Password   = '1Marmaduki';                               //SMTP password
-    $mail->SMTPSecure = 'tls';         
+    $mail->Username   = 'administrativo@gabrielachaluppe.com';                     //SMTP username
+    //$mail->Username   = 'gabriela.challupe.adv@gmail.com';                     //SMTP username
+   // $mail->Username   = 'gguicido.viana@gmail.com';                     //SMTP username
+    //$mail->Password   = 'Rafael10@';                               //SMTP password
+    $mail->Password   = '1Borbekai&';                               //SMTP password
+    $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Email marcado como "De"
-    $mail->setFrom('gguicido.viana@gmail.com', 'Mailer');
-	
-	//Email marcado como "Para"
-    $mail->addAddress('gguicido.viana@gmail.com', 'teste');     //Add a recipient
+    $mail->setFrom('administrativo@gabrielachaluppe.com', 'Administração');
+
+    //Email marcado como "Para"
+    $mail->addAddress('gabriela.challupe.adv@gmail.com', 'Gabriela');     //Add a recipient
 
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-	
-	//Assunto do email
-    $mail->Subject = 'Here is the subject';
-	
-	//Corpo do email
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    //Assunto do email
+    $mail->Subject = 'Dra Gabriela um novo cliente entrou em contato';
+
+    //Corpo do email
+    $mail->Body  = 
+    "<div display:block; border: 1px solid #000; background-color: #cfcfcf;>
+    <strong>Nome do cliente :</strong> $nome.<br>
+    <strong>contato que o cliente deixou :</strong> $email.<br>
+    <strong>assunto que ele deseja tratar :</strong> $assunto<br>
+    <strong>descrição :</strong> $message<br>
+    
+    </div>";
 
     $mail->send();
 
-	//Mensagem de Sucesso
-    echo 'Message has been sent';
+    echo "A sua menssagem foi enviada com sucesso";
 
-    
 } catch (Exception $e) {
-	
-	//Mensagem de erro
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 
-    header('Location: '. 'index.php' . '<div class="alert alert-danger" role="alert">
-    This is a danger alert—check it out!
-  </div>');
-
+    //Mensagem de erro
+    echo "os dados não foram enviados , ocoreeu uma falha na autenticação dos dados: {$mail->ErrorInfo}";
 }
--- index.php
+++ index.php
@@ -14,8 +14,7 @@
 	<!-- Font Awesome -->
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-		integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<!-- Slick slider -->
 	<link href="./assets/css/slick.css" rel="stylesheet">
 	<!-- Gallery Lightbox -->
@@ -34,9 +33,12 @@
 	<!-- Google Fonts Open sans -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,700,800" rel="stylesheet">
 
-	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-		integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
+
 <body>
 
 	<!--START SCROLL TOP BUTTON -->
@@ -54,9 +56,7 @@
 				<a class="navbar-brand mu-logo" href="index.html"><span></span></a>
 				<!-- image based logo -->
 				<!-- <a class="navbar-brand mu-logo" href="index.html"><img src="assets/images/logo.png" alt="logo"></a> -->
-				<button class="navbar-toggler" type="button" data-toggle="collapse"
-					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-					aria-label="Toggle navigation">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="fa fa-bars"></span>
 				</button>
 
@@ -64,10 +64,10 @@
 
 					<ul class="navbar-nav mr-auto mu-navbar-nav">
 						<li class="nav-item active">
-							<a href="index.html">Home</a>
+							<a href="index.php">Home</a>
 						</li>
 						<li class="nav-item"><a href="#mu-about">Sobre</a></li>
-						<li class="nav-item"><a href="#mu-back">Serviços</a></li>
+						<li class="nav-item"><a href="#area">Serviços</a></li>
 						<li class="nav-item"><a href="#form">Contato</a></li>
 					</ul>
 				</div>
@@ -143,145 +143,122 @@
 				</div>
 			</div>
 		</section>
-	
+
 		<!-- Call to Action -->
-		<article class="areas">
+		<articleclass="areas">
 			<div class="mu-back">
 				<div class="direito">
-					<img  style="width: 100%; height: 400px"id="section" src="assets/images/section.jpg">
-				<div style="margin-top: -370px; margin-bottom: 20px;">
-				<h3 class="tituloarea">Áreas de Atuação</h3>
-				<br>
-					<ul style="width: 300px; margin-left:auto; margin-right: auto;" class="atuacao">
-						<li style="text-align: start; margin-top: 20px; list-style: none;"> <i id="fas" class="fas fa-balance-scale-right"></i>Direito Criminal</li>
-						<li style="text-align: start; margin-top: 20px; list-style: none;"><i id="fas" class="fas fa-balance-scale"></i>Direito Civil</li>
-						<li style="text-align: start; margin-top: 20px; list-style: none;"><i id="fas" class="fas fa-balance-scale-right"></i>Direito do Trabalho</li>
-					</ul></div>
-				</div>
-			</div>
-		</article>
-		<!-- Call to Action -->
-	<section>
-		<div id="mu-call-to-action">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="mu-call-to-action-area">
-							<div class="mu-call-to-action-left">
-								<div class="mu-call-market">
-									<p>Entre em contato para mais informações.
-									</p>
-								</div>
-							</div>
-							<a id="btn-send" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=+5511913219644"
-								target="_blank" class="mu-btn mu-quote-btn"><strong>Entre em contato</strong><i
-									style="margin-left: 20px; color: #00cf23; font-size: 25p;"
-									class="fab fa-whatsapp"></i></a>
-						</div>
+					<img id="area" style="width: 100%; height: 600px" id="section" src="assets/images/section.jpg">
+					<div style="margin-top: -500px; margin-bottom: 40px;">
+						<h3 class="tituloarea">Áreas de Atuação</h3>
+						<br>
+						<ul style="width: 300px; margin-left:auto; margin-right: auto;" class="atuacao">
+							<li style="text-align: start; margin-top: 20px; list-style: none;"> <i id="fas" class="fas fa-balance-scale-right"></i>Direito Criminal</li>
+							<li style="text-align: start; margin-top: 20px; list-style: none;"><i id="fas" class="fas fa-balance-scale"></i>Direito Civil</li>
+							<li style="text-align: start; margin-top: 20px; list-style: none;"><i id="fas" class="fas fa-balance-scale-right"></i>Direito do Trabalho</li>
+						</ul>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-		<!-- Start Newsletter -->
-		<section id="mu-newsletter">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="mu-newsletter-area">
-							<!-- Title -->
-							<div class="row">
-								<div id="form" class="col-md-12">
-									<h3 class="contato_mensagem">Contato</h3>
+			</article>
+			<!-- Call to Action -->
+			<section style="margin-bottom: 40px;">
+				<div id="mu-call-to-action">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="mu-call-to-action-area">
+									<div class="mu-call-to-action-left">
+										<div class="mu-call-market">
+											<p>Entre em contato para mais informações.
+											</p>
+										</div>
+									</div>
+									<a id="btn-send" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=+5511913219644" target="_blank" class="mu-btn mu-quote-btn"><strong>Entre em contato</strong><i style="margin-left: 20px; color: #00cf23; font-size: 25p;" class="fab fa-whatsapp"></i></a>
 								</div>
-								<br>
-								<br>
-								<br>
-								<div class="col-md-12">
-									<section class="ftco-section">
-										<div class="container">
-											<div style="border: 3px solid rgba(201, 155, 56, 0.932)"class="row justify-content-center">
-												<div class="col-lg-10">
-													<div class="wrapper">
-														<div class="row no-gutters">
-															<div class="col-md-12 d-flex align-items-stretch">
-																<div class="contact-wrap w-100 p-md-5 p-4 py-5">
-																	<div id="form-message-warning" class="mb-4"></div>
-																	<div id="form-message-success" class="mb-4">
-																		<h5 class="mb-4">Entre em contato para mais
-																			informações</h5>
-																		Preencha o formulário abaixo e em breve nossa
-																		equipe entrará em contato
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Start Newsletter -->
+			<section id="mu-newsletter">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="mu-newsletter-area">
+								<!-- Title -->
+								<div class="row">
+									<div id="form" class="col-md-12">
+										<h3 class="contato_mensagem">Contato</h3>
+									</div>
+									<br>
+									<br>
+									<br>
+									<div class="col-md-12">
+										<section class="ftco-section">
+											<div class="container">
+												<div style="border: 3px solid rgba(201, 155, 56, 0.932)" class="row justify-content-center">
+													<div class="col-lg-10">
+														<div class="wrapper">
+															<div class="row no-gutters">
+																<div class="col-md-12 d-flex align-items-stretch">
+																	<div class="contact-wrap w-100 p-md-5 p-4 py-5">
+																		<div id="form-message-warning" class="mb-4"></div>
+																		<div id="form-message-success" class="mb-4">
+																			<h5 class="mb-4">Entre em contato para mais informações</h5>
+																			Preencha o formulário abaixo e em breve nossa
+																			equipe entrará em contato
 
-																	</div>
-
-																	<form method="POST" id="contactForm" name="contactForm" action="enviar.php">
-																		<div class="row">
-																			<div class="col-md-12">
-																				<div class="form-group">
-																					<input type="text"
-																						class="form-control" name="name"
-																						id="name"
-																						placeholder="Nome completo">
-																				</div>
-																			</div>
-																			<div class="col-md-12">
-																				<div class="form-group">
-																					<input type="email"
-																						class="form-control"
-																						name="email" id="email"
-																						placeholder="Seu melhor e-mail">
-																				</div>
-																			</div>
-																			<div class="col-md-12">
-																				<div class="form-group">
-																					<input type="text"
-																						class="form-control"
-																						name="subject" id="subject"
-																						placeholder="Assunto que deseja tratar">
-																				</div>
-																			</div>
-																			<div class="col-md-12">
-																				<div class="form-group">
-																					<textarea name="message"
-																						class="form-control"
-																						id="message" cols="30" rows="6"
-																						placeholder="Diga em poucas palavras por qual tipo de serviço você está buscando"></textarea>
-																				</div>
-																			</div>
-																			<div class="col-md-12">
-																				<div class="form-group">
-																					<input
-																						style="color:#000; font-weight: bold;"
-																						id="btn" type="submit"
-																						class="mu-btn mu-quote-btn"
-																						value="Enviar">
-																					<div class="submitting"></div>
-																				</div>
-																			</div>
 																		</div>
-																	</form>
+
+																		<div class="alert" id="alert" role="alert"></div>
+																		<br>
+																		<form id="name_form">
+																			<div class="row">
+																				<div class="col-md-12">
+																					<div class="form-group">
+																						<input type="text" class="form-control" name="name" id="nome" placeholder="Nome completo">
+																					</div>
+																				</div>
+																				<div class="col-md-12">
+																					<div class="form-group">
+																						<input type="email" class="form-control" name="email" id="email" placeholder="Seu melhor e-mail" required>
+																					</div>
+																				</div>
+																				<div class="col-md-12">
+																					<div class="form-group">
+																						<input type="text" class="form-control" name="subject" id="subject" placeholder="Assunto que deseja tratar">
+																					</div>
+																				</div>
+																				<div class="col-md-12">
+																					<div class="form-group">
+																						<textarea name="message" class="form-control" id="message" cols="30" rows="6" placeholder="Diga em poucas palavras por qual tipo de serviço você está buscando"></textarea>
+																					</div>
+																				</div>
+																				<div class="col-md-12">
+																					<div class="form-group">
+																						<input style="color:#000; font-weight: bold;" id="submit" type="button" class="mu-btn mu-quote-btn" value="Enviar">
+																					</div>
+																				</div>
+																			</div>
+																		</form>
+																		<div>
+																		</div>
+																	</div>
 																</div>
 															</div>
 														</div>
 													</div>
-												</div>
-											</div>
-										</div>
-									</section>
+										</section>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-		<!-- End Newsletter -->
+			</section>
 	</main>
-
-	<!-- End main content -->
-
-
 	<!-- Start footer -->
 	<footer id="mu-footer">
 		<div class="mu-footer-top">
@@ -290,7 +267,7 @@
 					<img class="mu-footer-logo" src="./assets/images/logo2.svg" alt="logo">
 				</div>
 			</div>
-			<p class="last-phrase"> “O advogado é indispensável à administração da justiça, sendo inviolável por seus
+			<p class="last-phrase"> O advogado é indispensável à administração da justiça, sendo inviolável por seus
 				atos e manifestações no exercício da profissão, nos limites da lei.” Art. 133, Constituição Federal/
 				1988
 			</p>
@@ -302,30 +279,95 @@
 						<div class="mu-footer-bottom-area">
 							<div class="mu-social-media">
 								<!-- <a class="mu-face" href="#"><i class="fab fa-facebook"></i></a> -->
-								<a class="mu-pinterest"
-									href="https://www.linkedin.com/in/gabriela-chaluppe-54488249/"><i
-										class="fab fa-linkedin"></i></a>
+								<a class="mu-pinterest" href="https://www.linkedin.com/in/gabriela-chaluppe-54488249/"><i class="fab fa-linkedin"></i></a>
 								<!-- <a class="mu-google-plus" href="#"><i class="fab fa-instagram"></i></a> -->
-								<a class="mu-youtube"
-									href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=+5511913219644"><i
-										class="fab fa-whatsapp"></i></a>
+								<a class="mu-youtube" href="https://api.whatsapp.com/send?1=pt_BR&amp;phone=+5511913219644"><i class="fab fa-whatsapp"></i></a>
 							</div>
 							<br>
 							<br>
 							<br>
 							<br>
-							<p class="mu-copy-right">&copy; Copyright <a rel="nofollow"
-									href="http://guilhermeviana.com/">Dra Gabriela Chaluppe</a>. Todos os direitos
+							<p class="mu-copy-right">&copy; Copyright <a rel="nofollow" href="http://guilhermeviana.com/">Dra Gabriela Chaluppe</a>. Todos os direitos
 								reservados.</p>
 						</div>
+
 					</div>
 				</div>
 			</div>
 		</div>
 
 	</footer>
+	<script type="text/javascript">
+		// const btn = document.getElementById("submit")
+		// btn.addEventListener('click' , () => {
+		// 	alert("clicou")
+		// })
+
+		$(document).ready(function() {
+			$('#submit').click(function() {
+
+				var nome = $('#nome').val();
+				var email = $('#email').val();
+				var subject = $('#subject').val();
+				var message = $('#message').val();
+
+				$('#alert').html('');
+				if (nome == '') {
+					$('#alert').html('Prencha seu nome');
+					$('#alert').addClass("alert-danger");
+
+					return false;
+				}
+				$('#alert').html('');
+
+				if (email == '') {
+					$('#alert').html('Prencha o email');
+					$('#alert').addClass("alert-danger");
+
+					return false;
+				}
+
+				$('#alert').html('');
+				if (subject == '') {
+					$('#alert').html('Prencha o assunto');
+					$('#alert').addClass("alert-danger");
+
+					return false;
+				}
+
+				$('#alert').html('');
+				if (message == '') {
+					$('#alert').html('Prencha a messagem');
+					$('#alert').addClass("alert-danger");
+
+					return false;
+				}
+				$('#alert').html('');
+
+				$.ajax({
+					url: 'enviar.php',
+					method: 'POST',
+					data: {
+						nome: nome,
+						email: email,
+						subject: subject,
+						message: message
+					},
+					success: function(result) {
+						$('form').trigger("reset");
+						$('#alert').addClass("alert-success");
+						$('#alert').fadeIn().html(result);
+						setTimeout(function() {
+							$('#alert').fadeOut('Slow')
+						}, 3000)
+					}
+				});
+			})
+		});
+	</script>
 	<!-- End footer -->
-	<script src="js/jquery.min.js"></script>
+
+
 	<script src="js/popper.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/jquery.validate.min.js"></script>
@@ -334,30 +376,9 @@
 	<!-- JavaScript -->
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-		integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-		crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-		integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-		crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.js"></script>
-	<!-- Slick slider -->
-	<script type="text/javascript" src="./assets/js/slick.min.js"></script>
-	<!-- Progress Bar -->
-	<script src="https://unpkg.com/circlebars@1.0.3/dist/circle.js"></script>
-	<!-- Filterable Gallery js -->
-	<script type="text/javascript" src="./assets/js/jquery.filterizr.min.js"></script>
-	<!-- Gallery Lightbox -->
-	<script type="text/javascript" src="./assets/js/jquery.magnific-popup.min.js"></script>
-	<!-- Counter js -->
-	<script type="text/javascript" src="./assets/js/counter.js"></script>
-	<!-- Ajax contact form  -->
-	<script type="text/javascript" src="./assets/js/app.js"></script>
-
-
-	<!-- Custom js -->
-	<script type="text/javascript" src="./assets/js/custom.js"></script>
-
 </body>
 
 </html>
